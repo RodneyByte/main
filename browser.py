576829bf5e2335f820dde461a3acbf441632f8f3
Rodney Thomas <84512619+RodneyByte@users.noreply.github.com>
2021-05-21 10:23:20 -0500

    Update browser.py

--- browser.py
+++ browser.py
@@ -19,18 +19,18 @@ def _translate(context, text, disambig):
 return QtGui.QApplication.translate(context, text, disambig)
 
 class BrowserDialog(object):
-def setupUi(self, Dialog):
-Dialog.setObjectName(_fromUtf8("Dialog"))
-Dialog.resize(1024, 768)
-self.qwebview = QWebView(Dialog)
-self.qwebview.setGeometry(QtCore.QRect(0, 50, 1020, 711))
-self.qwebview.setObjectName(_fromUtf8("kwebview"))
-self.lineEdit = QtGui.QLineEdit(Dialog)
-self.lineEdit.setGeometry(QtCore.QRect(10, 20, 1000, 25))
-self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+  def setupUi(self, Dialog):
+    Dialog.setObjectName(_fromUtf8("Dialog"))
+    Dialog.resize(1024, 768)
+    self.qwebview = QWebView(Dialog)
+    self.qwebview.setGeometry(QtCore.QRect(0, 50, 1020, 711))
+    self.qwebview.setObjectName(_fromUtf8("kwebview"))
+    self.lineEdit = QtGui.QLineEdit(Dialog)
+    self.lineEdit.setGeometry(QtCore.QRect(10, 20, 1000, 25))
+    self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
 
-self.retranslateUi(Dialog)
-QtCore.QMetaObject.connectSlotsByName(Dialog)
+    self.retranslateUi(Dialog)
+    QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-def retranslateUi(self, Dialog):
-Dialog.setWindowTitle(_translate("Browser", "Browser", None))
+  def retranslateUi(self, Dialog):
+    Dialog.setWindowTitle(_translate("Main - Simple, Privacy", "Main Browser", None))
